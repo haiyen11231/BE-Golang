65f215cbba6522df4509d40d07c454a053994b4e
haiyen11231 <haiyen11231@gmail.com>
2024-12-24 03:18:00 +0800

    :white_check_mark: docs: update template and requirement

--- Exercise_4.go
+++ Exercise_4.go
@@ -1,10 +1,10 @@
-// 	4. Given a file a.txt, write a function that returns a slice of structs representing people, with information extracted from the file. The requirements are:
+// 4. Given a file a.txt, write a function that returns a slice of structs representing people, with information extracted from the file. The requirements are:
 // The name must be converted to all uppercase.
 // The profession must be converted to all lowercase.
 // (Refer to file reading instructions at: https://zetcode.com/golang/readfile/).
-// 	Tom Software engineer|1995
-// 	John Snow Teacher|1997
+// 	Tom|Software engineer|1995
+// 	John Snow|Teacher|1997
 // 	Maria Onitsuka|Actor|1993
-// 	Emil Football player|1987
+// 	Emil|Football player|1987
 
 package main
