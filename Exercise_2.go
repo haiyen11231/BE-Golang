a3014a2eb92f87d3e57e5efe56017bc0e7fc2af9
haiyen11231 <haiyen11231@gmail.com>
2024-12-25 17:16:56 +0800

    ✨ feat: add Exercise_2

--- Exercise_2.go
+++ Exercise_2.go
@@ -3,3 +3,20 @@
 // 	The value is the number of times that character appears in the string.
 
 package main
+
+import "fmt"
+
+func getCharOccurrences (s string) map[string]int {
+	m := make(map[string]int)
+	for idx := range s {
+		m[string(s[idx])]++
+	}
+	return m
+}
+
+func Exercise_2() {
+	var s string
+	fmt.Println("Enter a string: ")
+	fmt.Scan(&s)
+	fmt.Println("Character occurences:", getCharOccurrences(s))
+}
--- main.go
+++ main.go
@@ -5,10 +5,10 @@ import (
 )
 
 func main() {
-	fmt.Println("----------Exercise 1----------")
-	Exercise_1()
-	// fmt.Println("----------Exercise 2----------")
-	// Exercise_2()
+	// fmt.Println("----------Exercise 1----------")
+	// Exercise_1()
+	fmt.Println("----------Exercise 2----------")
+	Exercise_2()
 	// fmt.Println("----------Exercise 3----------")
 	// Exercise_3()
 	// fmt.Println("----------Exercise 4----------")
