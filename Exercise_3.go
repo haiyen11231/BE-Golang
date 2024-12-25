aa0ff6cec647bb9700582d2407d1b830a6f3ea3f
haiyen11231 <haiyen11231@gmail.com>
2024-12-25 17:43:28 +0800

    ✨ feat: add Exercise_3

--- Exercise_3.go
+++ Exercise_3.go
@@ -1,3 +1,18 @@
 // 3. Write a function to solve the "Two Sum" problem: https://leetcode.com/problems/two-sum/ using a map.
 
 package main
+
+import "fmt"
+
+func Exercise_3(nums []int, target int) []int {
+    m := make(map[int]int)
+    for idx, num := range nums {
+        if val, exists := m[num]; exists {
+            fmt.Println(idx, val)
+            return []int{idx, val}
+        } else {
+            m[target - num] = idx
+        }
+    }
+    return nil
+}
--- main.go
+++ main.go
@@ -7,10 +7,10 @@ import (
 func main() {
 	// fmt.Println("----------Exercise 1----------")
 	// Exercise_1()
-	fmt.Println("----------Exercise 2----------")
-	Exercise_2()
-	// fmt.Println("----------Exercise 3----------")
-	// Exercise_3()
+	// fmt.Println("----------Exercise 2----------")
+	// Exercise_2()
+	fmt.Println("----------Exercise 3----------")
+	Exercise_3([]int{4, 3, 1, 8, 3, 9}, 10)
 	// fmt.Println("----------Exercise 4----------")
 	// Exercise_4()
 }
